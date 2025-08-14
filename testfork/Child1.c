f9a6e1cb9e81672e880a43194b0912beb4e3e14c
SupanudThuvichain <674244123@mail.pbru.ac.th>
2025-08-14 11:58:48 +0700

    Add files via upload

---
+++ testfork/Child1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main() {
+    printf("My bio.: \n");
+    printf("ID : 674244123 \n");
+    printf("Name : Supaund Thuvichain\n");
+    printf("------------------------------\n");
+    getchar();
+}
